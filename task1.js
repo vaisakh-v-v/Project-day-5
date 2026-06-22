eb869f4f180dba4b7c2180a9240b886caa8991e4
vaisakh-v-v <vaisakh.v@vonnue.com>
2026-06-22 13:04:11 +0530

    Feature:completed task-1 of week 3 day 1
    compared different data types and typcasting technique

---
+++ task1.js
@@ -0,0 +1,34 @@
+//object 
+console.log(typeof null);
+//object 
+console.log(typeof []);
+//object 
+console.log(typeof {});
+//Number
+console.log(typeof NaN);
+//funtion
+console.log(typeof function(){});
+//true
+console.log(0==false);
+//true
+console.log(''== false);
+//true
+console.log(null==undefined);
+//true
+console.log(null===undefined);
+//false
+console.log(NaN===NaN);
+//12
+console.log(1+'2');
+//2
+console.log('3' - 1);
+//2
+console.log(true + true);
+//array
+console.log([] + []); 
+//string   
+console.log([] + {});
+
+
+
+
